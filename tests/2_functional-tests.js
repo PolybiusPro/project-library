5dd791e39c13469585cf7005716ce12885bcddde
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-21 12:56:38 -0500

    fix: comment out sample test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -20,7 +20,7 @@ suite("Functional Tests", function () {
      * ----[EXAMPLE TEST]----
      * Each test should completely test the response of the API end-point including response status code!
      */
-    test("#example Test GET /api/books", function (done) {
+    /* test("#example Test GET /api/books", function (done) {
         chai.request(server)
             .get("/api/books")
             .end(function (err, res) {
@@ -46,7 +46,7 @@ suite("Functional Tests", function () {
                 );
                 done();
             });
-    });
+    }); */
     /*
      * ----[END of EXAMPLE TEST]----
      */
